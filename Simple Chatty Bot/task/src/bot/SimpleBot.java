3b3854dc9ab16cd20bc917bdcd3fc0243841e987
Karolina-LK <lubieniecka@interia.pl>
2020-12-22 18:56:45 +0100

    refactor previous solution

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -3,18 +3,23 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
+    private static final int CORRECT = 2;
+
+    public static void showMessage(String text){
+        System.out.println(text);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2020.");
-        System.out.println("Please, remind me your name.");
+        showMessage("Hello! My name is Aid.");
+        showMessage("I was created in 2020.");
+        showMessage("Please, remind me your name.");
 
         String name = scanner.nextLine();
 
-        System.out.println("What a great name you have, " + name + "!");
-        System.out.println("Let me guess your age.");
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        showMessage("What a great name you have, " + name + "!");
+        showMessage("Let me guess your age.");
+        showMessage("Enter remainders of dividing your age by 3, 5 and 7.");
 
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
@@ -22,18 +27,18 @@ public class SimpleBot {
 
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
-        System.out.println("Your age is " + age + "; that's a good time to start programming!");
-        System.out.println("Now I will prove to you that I can count to any number you want.");
+        showMessage("Your age is " + age + "; that's a good time to start programming!");
+        showMessage("Now I will prove to you that I can count to any number you want.");
 
         int providedNo = scanner.nextInt();
         int i = 0;
         while (i <= providedNo) {
-            System.out.println(i + "!");
+            showMessage(i + "!");
             i++;
         }
 
-        System.out.println("Let's test your programming knowledge.");
-        System.out.println("Why do we use methods?\n" +
+        showMessage("Let's test your programming knowledge.");
+        showMessage("Why do we use methods?\n" +
                 "1. To repeat a statement multiple times.\n" +
                 "2. To decompose a program into several small subroutines.\n" +
                 "3. To determine the execution time of a program.\n" +
@@ -41,16 +46,35 @@ public class SimpleBot {
         String resultNeg = "Please, try again.";
         while(scanner.hasNext()) {
             int answer = scanner.nextInt();
-            if (answer == 1) {
-                System.out.println(resultNeg);
-            } else if (answer == 2) {
-                System.out.println("Congratulations, have a nice day!");
-                break;
-            } else if (answer == 3) {
-                System.out.println(resultNeg);
-            } else if (answer == 4) {
-                System.out.println(resultNeg);
+
+            if (answer == CORRECT) {
+                showMessage("Congratulations, have a nice day!");
+            } else {
+                showMessage(resultNeg);
             }
+
+//            if (answer == 1) {
+//                showMessage(resultNeg);
+//            } else if (answer == 2) {
+//                showMessage("Congratulations, have a nice day!");
+//                break;
+//            } else if (answer == 3) {
+//                showMessage(resultNeg);
+//            } else if (answer == 4) {
+//                showMessage(resultNeg);
+//            }
         }
     }
+//    public enum Answer{
+//        WRONG1(1), CORRECT2(2), WRONG3(3), WRONG4(4);
+//        private int id;
+//
+//        Answer(int id) {
+//            this.id = id;
+//        }
+//
+//        public int getId() {
+//            return id;
+//        }
+//    }
 }
